cd268d9342e315edabfa386c2cd2bf5c71acadff
kethusha <kethusa01@gmail.com>
2024-12-30 03:52:50 +0000

    here n1,n2 are position parameters

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main(){
+  addTwoNUmber(10,20);
   runApp(MaterialApp(
     home:HelloWorldApp(),
   ));
   
+}
+void addTwoNUmber(int n1,int n2){
+  int sum=n1+n2;
+  print(sum);
 }
 class HelloWorldApp extends StatelessWidget {
   const HelloWorldApp({super.key});
