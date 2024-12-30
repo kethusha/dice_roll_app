e5aa4d48a92fbf7ee5d4c6136f0313545f5f53a9
kethusha <kethusa01@gmail.com>
2024-12-30 04:42:13 +0000

    create  statelesswidget called HelloWorldApp and added app Bar

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  addTwoNUmber(n2:10,n1:20);
+  
   runApp(MaterialApp(
     home:HelloWorldApp(),
   ));
   
-}
-void addTwoNUmber({required n1,required n2}){
-  int sum=n1+n2;
-  print(sum);
-}
-class HelloWorldApp extends StatelessWidget {
+}class HelloWorldApp extends StatelessWidget {
   const HelloWorldApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(
+        title:  Text('This is the appbar'),
+        backgroundColor: Colors.blue,
+      ),
+    );
   }
 }
