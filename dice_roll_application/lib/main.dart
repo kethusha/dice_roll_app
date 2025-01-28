a4651a07537234b73ff433dcf30c7d68447c086e
kethusha <kethusa01@gmail.com>
2025-01-28 04:23:35 +0000

    complete the dice roll app screen

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -3,19 +3,51 @@ import 'package:flutter/material.dart';
 void main(){
   
   runApp(MaterialApp(
-    home:HelloWorldApp(),
+    home:DiceRollApp(),
   ));
   
-}class HelloWorldApp extends StatelessWidget {
-  const HelloWorldApp({super.key});
+}class DiceRollApp extends StatelessWidget {
+  const DiceRollApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(
-        title:  Text('This is the appbar'),
-        backgroundColor: Colors.blue,
+      backgroundColor: Colors.grey.shade200,
+      body: Container(
+        //decoration: BoxDecoration(color: Colors.red.shade400),
+        //width:300,
+        width: MediaQuery.of(context).size. width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Dice Roll App!',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w800,
+              color: Colors.black.withOpacity(0.41)
+              ),),
+              Image.asset('assets/images/dice-1.png'),
+              Container(
+                height: 51,
+                width: MediaQuery.of(context).size.width*0.7,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 29, 46, 29),
+                  borderRadius: BorderRadius.circular(12.0)),
+                  child:Center(
+                    child: Text(
+                      'Roll Dise',
+                      style: TextStyle(color: Colors.white,fontSize:20),
+                      )
+                  ),
+        
+                
+              
+              ),
+          ],
+        ),
       ),
-    );
+      );
+    
   }
 }
